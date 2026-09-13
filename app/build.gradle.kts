b89738172043548a8c8ad1bd9745ea6196c9da75
subhojit paul <subhojitpaul26042004@gmail.com>
2026-09-13 18:17:37 +0530

    chore(release): bump version to v1.6.3 (27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.subhojitscribe.dasmo.in"
     minSdk = 24
     targetSdk = 36
-    versionCode = 26
-    versionName = "1.6.2"
+    versionCode = 27
+    versionName = "1.6.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
